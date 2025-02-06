7a3772b0b9a828fc9ea6cc34b2b7af357a930303
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-02-06 21:00:15 +0800

    - add this song

--- api/user.js
+++ api/user.js
@@ -4,7 +4,22 @@ export default function handler(req, res) {
     {
       id:1,
       name:"Crazy",
-      image:""
+      image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/crazy-lesserafim.png?raw=true"
+    },
+    {
+      id:2,
+      name: "1-800 hot n fun",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/1800%20hot%20n%20fun.png?raw=true"
+    },
+    {
+      id:3,
+      name: "Smart",
+      image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/smart.png?raw=true"
+    },
+    {
+      id:4,
+      name:"Whiplash",
+      image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/whiplash.png?raw=true"
     }
   ]
   res.status(200).json(musicaInfo);
