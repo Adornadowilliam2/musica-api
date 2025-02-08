35814ede54c6d3d45414e13972fd6dc599e03665
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-02-08 21:19:18 +0800

    - adding new song

--- api/user.js
+++ api/user.js
@@ -21,21 +21,27 @@ export default function handler(req, res) {
     },
     {
       id:4,
-      name:"Whiplash",
-      image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/whiplash.png?raw=true",
-      music:"https://github.com/Adornadowilliam2/kpop-song/blob/main/aespa%20%EC%97%90%EC%8A%A4%ED%8C%8C%20'Whiplash'%20MV%20%5BjWQx2f-CErU%5D.mp3?raw=true"
-    },
-    {
-      id:5,
       name:"Antifragile",
       image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/antifragile.png?raw=true",
       music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/LE%20SSERAFIM%20(%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C)%20'ANTIFRAGILE'%20OFFICIAL%20M%E2%A7%B8V%20%5Bpyf8cbqyfPs%5D.mp3?raw=true"
     },
     {
-      id:6,
+      id:5,
       name:"Perfect Night",
       image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/perfectnight.png?raw=true",
       music:"https://github.com/Adornadowilliam2/kpop-song/blob/main/LE%20SSERAFIM%20(%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C)%20'Perfect%20Night'%20OFFICIAL%20M%E2%A7%B8V%20with%20OVERWATCH%202%20(Choreography%20ver.)%20%5BoKBwWQI-IoI%5D.mp3?raw=true"
+    },
+    {
+      id:6,
+      name:"Whiplash",
+      image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/whiplash.png?raw=true",
+      music:"https://github.com/Adornadowilliam2/kpop-song/blob/main/aespa%20%EC%97%90%EC%8A%A4%ED%8C%8C%20'Whiplash'%20MV%20%5BjWQx2f-CErU%5D.mp3?raw=true"
+    },
+    {
+      id:6,
+      name:"Drama",
+      image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/drama.png?raw=true",
+      music:"https://github.com/Adornadowilliam2/kpop-song/blob/main/aespa(%EC%97%90%EC%8A%A4%ED%8C%8C)%20-%20Drama%20%40%EC%9D%B8%EA%B8%B0%EA%B0%80%EC%9A%94%20inkigayo%2020231112%20%5B1c_3pK15yWg%5D.mp3?raw=true"
     },
     {
       id:7,
@@ -51,18 +57,30 @@ export default function handler(req, res) {
     },
     {
       id:9,
+      name:"Marilag",
+      image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/marilag.png?raw=true",
+      music:"https://github.com/Adornadowilliam2/opm-song/blob/main/Marilag%20(Live%20at%20The%20Cozy%20Cove)%20-%20Dionela%20%5BbD9whtdq7dw%5D.mp3?raw=true"
+    },
+    {
+      id:10,
+      name:"Universe",
+      image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/universe.png?raw=true",
+      music:"https://github.com/Adornadowilliam2/anime-song/blob/main/Official%E9%AB%AD%E7%94%B7dism%20-%20Universe%EF%BC%BBOfficial%20Live%20Video%EF%BC%BD%20%5BRvwc5_PpLuY%5D.mp3?raw=true"
+    },
+    {
+      id:11,
       name:"Geronimo",
       image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/geronimo.png?raw=true",
       music:"https://github.com/Adornadowilliam2/nostalgic-song/blob/main/Sheppard%20-%20Geronimo%20(Official%20Music%20Video)%20%5BUL_EXAyGCkw%5D.mp3?raw=true"
     },
     {
-      id:10,
+      id:12,
       name:"Fresh eyes",
       image:"https://github.com/Adornadowilliam2/shuffle-react/blob/main/fresheyes-andy.png?raw=true",
       music:"https://github.com/Adornadowilliam2/nostalgic-song/blob/main/Andy%20Grammer%20-%20Fresh%20Eyes%20(Official%20Music%20Video)%20%5B5bgemCaaQkU%5D.mp3?raw=true"
     },
     {
-      id:11,
+      id:13,
       name:"Run sakamoto run",
       image:'https://github.com/Adornadowilliam2/shuffle-react/blob/main/runsakamotorun-vaundy.png?raw=true',
       music:"https://github.com/Adornadowilliam2/anime-song/blob/main/%E8%B5%B0%E3%82%8CSAKAMOTO%20%E2%80%93%20RUN%20SAKAMOTO%20RUN%20%E2%80%93%20Opening%20theme%20to%20SAKAMOTO%20DAYS%20%5Buj1DkgXSR4w%5D.mp3?raw=true"
