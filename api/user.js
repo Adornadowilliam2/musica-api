a0ab34dcc2a5577cb81aa4375b6d767be2ea4129
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-02-09 19:37:34 +0800

    - stfu

--- api/user.js
+++ api/user.js
@@ -1,114 +1,104 @@
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  const courseList = [
+  
+  const musicInfo = [
     {
-      id:1,
-      name:"Advanced Gas Metal Arc Welding",
-      description:"This module is designed for welders who would like to take further their welding skills in groove welding of carbon steel pipes using Gas Metal Arc Welding process. Practical exercises are designed to develop manual skills necessary to produce quality groove welds on pipes in all types of welding position.",
-      price:"₱10,000",
-      duration:"40 hours",
-      link:"https://courselist.mfi.org.ph/courses/1180",
-      image:"https://media.licdn.com/dms/image/v2/C5603AQEYx5k_-qrXcQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1609910450664?e=2147483647&v=beta&t=bvrc339QHKFsD1kxuGeAwpAz3sZF9o3yXIk7OIDWNig"
+      id: 1,
+      name: "Crazy",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/crazy-lesserafim.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/LE%20SSERAFIM%20(%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C)%20'CRAZY'%20OFFICIAL%20MV%20%5Bn6B5gQXlB-0%5D.mp3?raw=true"
     },
     {
-      id:2,
-      name:"Advanced Graphic Design Using Photoshop & Illustrator (Online Course)",
-      description:"An advanced short intensive course aiming to teach young individuals about the basics and importance of Graphic Designing and its practical uses in the creative and working industry.",
-      price:"₱1,750",
-      duration:"9 hours",
-      link:"https://courselist.mfi.org.ph/courses/2655",
-      image:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTo33YcutzA1p0mRjKqsm6b1B5XpKRJI9BYr5-7lCIg2UztUTdj"
+      id: 2,
+      name: "1-800 hot n fun",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/1800%20hot%20n%20fun.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/1-800-hot-n-fun%20%5BrGD5U8u1Dk0%5D.mp3?raw=true"
     },
     {
-      id:3,
-      name:"Advanced Industrial Motor Control",
-      description:"The Advanced Industrial Motor Control course is designed for electrical technicians, engineers, and maintenance personnel who are responsible for designing, installing, maintaining, and troubleshooting complex motor control systems in industrial settings. This course builds upon foundational knowledge of motor control principles and focuses on advanced concepts, technologies, and applications relevant to industrial automation and control. Participants will explore advanced topics in motor control theory, learn about the latest technologies and techniques in motor control systems, and gain hands-on experience with programming, configuring, and optimizing motor control systems using industry-standard equipment and software. Emphasis will be placed on practical applications, troubleshooting methodologies, and safety considerations in industrial environments.",
-      price:"₱9,000",
-      duration:"48 hours",
-      link:"https://courselist.mfi.org.ph/courses/65",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT70cNDrNfd0qSA8S_4bfttXBvLPaLxJG3OP9hEv1QMAIh0feB2"
+      id: 3,
+      name: "Smart",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/smart.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/LE%20SSERAFIM%20(%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C)%20'Smart'%20OFFICIAL%20MV%20%5BKNexS61fjus%5D.mp3?raw=true"
     },
     {
-      id:4,
-      name:"Advanced Industrial Motor Control (Pasay Branch)",
-      description:"This module deals with the study of electric motors (AC and DC) and the associated control circuits. It also includes an understanding and practical use of Programmable Controllers and Variable Frequency Drives. Troubleshooting and design of motor control circuits are also given emphasis. Laboratory activities are provided to verify and practice learned principles.",
-      price:"₱9,000",
-      duration:"48 Hours",
-      link:"https://courselist.mfi.org.ph/courses/1523",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT70cNDrNfd0qSA8S_4bfttXBvLPaLxJG3OP9hEv1QMAIh0feB2"
+      id: 4,
+      name: "Antifragile",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/antifragile.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/LE%20SSERAFIM%20(%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C)%20'ANTIFRAGILE'%20OFFICIAL%20M%E2%A7%B8V%20%5Bpyf8cbqyfPs%5D.mp3?raw=true"
     },
     {
-      id:7,
-      name:"Advanced MS Excel Course (Online)",
-      description:"Trainees are expected to efficiently manage spreadsheets and analyze data for business presentations, data storage and analytical representation using one of the world’s most popular spreadsheet application",
-      price:"₱3,500",
-      duration:"8 hours",
-      link:"https://courselist.mfi.org.ph/courses/2661",
-      image:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSC6OyFSCCZvPDl9I0A0Vd9QBgG9mD_T3eNmGQ3_-UHy2vqLYEn"
+      id: 5,
+      name: "Perfect Night",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/perfectnight.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/LE%20SSERAFIM%20(%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C)%20'Perfect%20Night'%20OFFICIAL%20M%E2%A7%B8V%20with%20OVERWATCH%202%20(Choreography%20ver.)%20%5BoKBwWQI-IoI%5D.mp3?raw=true"
     },
     {
-      id:8,
-      name:"Advanced MS Excel Course",
-      description:"Trainees are expected to efficiently manage spreadsheets and analyze data for business presentations, data storage and analytical representation using one of the world’s most popular spreadsheet application",
-      price:"₱7,200",
-      duration:"21 hours",
-      link:"https://courselist.mfi.org.ph/courses/1753",
-      image:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSC6OyFSCCZvPDl9I0A0Vd9QBgG9mD_T3eNmGQ3_-UHy2vqLYEn"
+      id: 6,
+      name: "Whiplash",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/whiplash.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/aespa%20%EC%97%90%EC%8A%A4%ED%8C%8C%20'Whiplash'%20MV%20%5BjWQx2f-CErU%5D.mp3?raw=true"
+    },
+    {
+      id: 7,
+      name: "Drama",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/drama.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/aespa(%EC%97%90%EC%8A%A4%ED%8C%8C)%20-%20Drama%20%40%EC%9D%B8%EA%B8%B0%EA%B0%80%EC%9A%94%20inkigayo%2020231112%20%5B1c_3pK15yWg%5D.mp3?raw=true"
+    },
+    {
+      id: 8,
+      name: "Strategy",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/strategy-twice.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/kpop-song/blob/main/TWICE%20%E2%80%9CStrategy%20(feat.%20Megan%20Thee%20Stallion)%E2%80%9D%20M%E2%A7%B8V%20%5BSz_wWzgh-vQ%5D.mp3?raw=true"
     },
     {
       id: 9,
-      name:"Advanced Shielded Metal Arc Welding (Pipe Welding)",
-      description:"This course focuses on advanced techniques in shielded metal arc welding (SMAW) specifically for pipe welding applications. Participants will learn about various pipe welding positions, joint designs, and the properties of different welding electrodes. The curriculum emphasizes safety practices and quality control measures. Hands-on training will enable participants to develop precision welding skills on various types of pipes and materials. By the end of the course, participants will be prepared to tackle complex pipe welding projects in various industrial settings.",
-      price:"₱10,000",
-      duration:"40 hours",
-      link:"https://courselist.mfi.org.ph/courses/1721",
-      image:"https://i.ytimg.com/vi/I1P5GuVHFQA/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGGMgYyhjMA8=&rs=AOn4CLAQ76Iel9wj6Y_iUcJcbI_M6e3nEg"
+      name: "Nobody",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/nobody-onerepublic.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/anime-song/blob/main/OneRepublic%20-%20Nobody%20(from%20Kaiju%20No.%208)%20%5BOfficial%20Lyric%20Video%5D%20%5BChukpOHfAI8%5D.mp3?raw=true"
     },
     {
-      id:10,
-      name:"Applied Industrial Electricity (PEE / BWIM / IE / AIMC)",
-      description:"Applied Industrial Electricity focuses on the practical application of electrical principles in an industrial setting. It combines elements from electrical theory, systems, and controls, emphasizing real-world applications in industrial environments. Participants will gain hands-on experience with electrical components, safety practices, and troubleshooting techniques essential for maintaining and optimizing industrial electrical systems.",
-      price:"₱31,500",
-      duration:"200 Hours",
-      link:"https://courselist.mfi.org.ph/courses/3081",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxP-xvKDqLljUn9myHNyR2J2DLYyyh0Kbt5Q&s"
+      id: 10,
+      name: "Marilag",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/marilag.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/opm-song/blob/main/Marilag%20(Live%20at%20The%20Cozy%20Cove)%20-%20Dionela%20%5BbD9whtdq7dw%5D.mp3?raw=true"
     },
     {
-      id:11,
-      name:"AutoCAD",
-      description:"This course introduces the students to AUTOCAD by learning the features, commands, techniques for creating, and editing drawing. Student will be able to create 2D and 3D drawings. They will have an opportunity to work on drawings used in various technical fields, such as mechanical parts, electrical plans and schematics, architectural plans and other engineering diagrams. The participants will receive “hands-on” training and will develop the techniques that are essentials in today’s job market. They will learn on how to adapt basic technical drafting techniques to computer generated drawings of various drafting disciplines.",
-      price:"₱5,800",
-      duration:"60 Hours",
-      link:"https://courselist.mfi.org.ph/courses/57",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUn9pJ7BttcPy4UqLi7PnlvVbTR6NtakOIJo8vkxWLGef1kaph"
+      id: 11,
+      name: "Sining",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/sining.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/opm-song/blob/main/Dionela%20-%20sining%20ft.%20Jay%20R%20%5BLLqDfGFMJbk%5D.mp3?raw=true"
     },
     {
-      id:12,
-      name:"AutoCAD (Pasay Branch)",
-      description:"This course introduces the students to AUTOCAD by learning the features, commands, techniques for creating, and editing drawing. Student will be able to create 2D and 3D drawings. They will have an opportunity to work on drawings used in various technical fields, such as mechanical parts, electrical plans and schematics, architectural plans and other engineering diagrams. The students will receive “hands-on” training and will develop the techniques that are essentials in today’s job market. They will learn on how to adapt basic technical drafting techniques to computer generated drawings of various drafting disciplines.",
-      price:"₱5,800",
-      duration:"60 Hours",
-      link:"https://courselist.mfi.org.ph/courses/1499",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUn9pJ7BttcPy4UqLi7PnlvVbTR6NtakOIJo8vkxWLGef1kaph"
+      id: 12,
+      name: "Universe",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/universe.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/anime-song/blob/main/Official%E9%AB%AD%E7%94%B7dism%20-%20Universe%EF%BC%BBOfficial%20Live%20Video%EF%BC%BD%20%5BRvwc5_PpLuY%5D.mp3?raw=true"
     },
     {
-      id:13,
-      name:"AutoCAD CIVIL 3D",
-      description:"AutoCAD Civil 3D course provides comprehensive training in the use of Autodesk's specialized software for civil engineering design and drafting. Participants learn to navigate the AutoCAD Civil 3D interface, create and edit essential civil objects such as points, alignments, and profiles, and develop proficiency in surface modeling for grading and earthwork calculations. The course covers key aspects of alignment and corridor design for roads and highways, as well as the creation and analysis of pipe and pressure networks for utility systems. Participants gain hands-on experience in quantity takeoff, reporting, and efficient project workflow, emphasizing best practices for data interoperability and collaboration with industry-standard formats. Throughout the course, practical exercises, real-world projects, and case studies reinforce the application of AutoCAD Civil 3D skills, ensuring participants are well-equipped for civil engineering projects in diverse professional settings.",
-      price:"₱6,000",
-      duration:"60 Hours",
-      link:"https://courselist.mfi.org.ph/courses/3054",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_cnFgSNE4gweh-usL_dUBTWtltqWHTp46Vw&s"
+      id: 13,
+      name: "Geronimo",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/geronimo.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/nostalgic-song/blob/main/Sheppard%20-%20Geronimo%20(Official%20Music%20Video)%20%5BUL_EXAyGCkw%5D.mp3?raw=true"
     },
     {
-      id:14,
-      name:"Automotive Electricity Course",
-      description:"The Automotive Electricity course is designed for individuals seeking to gain a thorough understanding of electrical systems and their applications in modern vehicles. This course provides foundational knowledge and practical skills necessary for diagnosing, repairing, and maintaining automotive electrical systems. Participants will explore key concepts in automotive electricity, including wiring diagrams, circuit analysis, component functionality, and troubleshooting techniques. With a focus on hands-on learning, the course includes practical exercises and real-world scenarios to help participants develop the competence needed to work effectively with automotive electrical systems. By the end of the course, participants will be prepared to handle a wide range of electrical issues and perform essential maintenance and repairs in automotive settings.",
-      price:"₱7,000",
-      duration:"40 Hours",
-      link:"https://courselist.mfi.org.ph/courses/3083",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThdjB1Pczp2-eJIRFbyzxOxFd5eGnysESIV3Ont6QnMfLOTgEf"
+      id: 14,
+      name: "Viva la vida",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/vivalavida.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/nostalgic-song/blob/main/Coldplay%20-%20Viva%20La%20Vida%20(Official%20Video)%20%5BdvgZkm1xWPE%5D.mp3?raw=true"
+    },
+    {
+      id: 15,
+      name: "Fresh eyes",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/fresheyes-andy.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/nostalgic-song/blob/main/Andy%20Grammer%20-%20Fresh%20Eyes%20(Official%20Music%20Video)%20%5B5bgemCaaQkU%5D.mp3?raw=true"
+    },
+    {
+      id: 16,
+      name: "Run sakamoto run",
+      image: "https://github.com/Adornadowilliam2/shuffle-react/blob/main/runsakamotorun-vaundy.png?raw=true",
+      music: "https://github.com/Adornadowilliam2/anime-song/blob/main/%E8%B5%B0%E3%82%8CSAKAMOTO%20%E2%80%93%20RUN%20SAKAMOTO%20RUN%20%E2%80%93%20Opening%20theme%20to%20SAKAMOTO%20DAYS%20%5Buj1DkgXSR4w%5D.mp3?raw=true"
     }
-  ] 
-  res.status(200).json(courseList);
+  ];
+
+  res.status(200).json(musicInfo);
 }
